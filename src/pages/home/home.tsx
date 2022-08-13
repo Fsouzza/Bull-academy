3f6451d4b66059ce56dd600527e9acaa632f1f7b
F. Souzza <filipesouzza.adm@gmail.com>
2022-08-13 12:12:22 -0300

    criacao page home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,64 @@
-import { Header } from 'components/header';
-
+import styles from './Home.module.scss';
+import heart from 'assets/img/icon/heart.svg';
+import frontImg from 'assets/img/front.png';
+import back from 'assets/img/back.png';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return(
-    <div>
-      <Header />
-      <h1>Inicio</h1>
-    </div>
+    <header className={styles.header}>
+      <div className={styles.header__left}>
+        <div className={styles.header__left__ad}>
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
+
+        <div className={styles.header__left__text}>
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div className={styles.header__left__description}>
+            <span>
+              In here we will help you to shape and build your idel body and live up your life to fullest
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.header__left__figures}>
+          <div>
+            <span>+50</span>
+            <p>Expert Coaches</p>
+          </div>
+          <div>
+            <span>+1K</span>
+            <p>Members joined</p>
+          </div>
+          <div>
+            <span>+60</span>
+            <p>Fitness programs</p>
+          </div>  
+        </div>
+
+        <div className={styles.header__left__buttons}>
+          <Link className={styles.header__left__btn} to='/exercicios'>Get Started</Link>
+          <Link className={styles.header__left__btn} to='/planos'>To sign</Link>
+        </div>
+      </div>
+
+      <div className={styles.header__right}>
+        <div className={styles.header__right__heartRate}>
+          <img className={styles.header__right__img} src={heart} alt='' />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+        <img src={frontImg} alt='' className={styles.header__right__frontimage} />
+        <img src={back} alt='' className={styles.header__right__back} />
+        <div className={styles.header__right__backimage} />
+      </div>
+    </header>
   );
 };
